86b846b0ac0c703e08103e53af4aba8259c53db1
marco_polo <nol-95@mail.ru>
2021-07-06 17:33:02 +0300

    f

--- src/lesso5/persson.java
+++ src/lesso5/persson.java
@@ -10,15 +10,20 @@ public class persson {
     private int money;
     private int age;
 
-
-    public persson( String FIO, String position, String email , int phone, int money, int age){
+    public persson(String FIO, String position, String email , int phone, int money, int age) {
         this.FIO = FIO;
         this.position = position;
         this.email = email;
+        this.money= money;
         this.phone = phone;
         this.age = age;
+
+    }
+    public void info (){
+        System.out.println(FIO+position+email+phone+money+age);
     }
 
+    int getAge() {return age;};
 
 }
 
