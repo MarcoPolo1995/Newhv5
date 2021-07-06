cecaa47b46eb22525139621e48de4ab8d0b3bac7
marco_polo <nol-95@mail.ru>
2021-07-06 15:35:18 +0300

    f

--- src/lesso5/persson.java
+++ src/lesso5/persson.java
@@ -2,9 +2,25 @@ package lesso5;
 
 public class persson {
 
+    private String FIO;
+    private String position;
+    private String email;
 
-2222
+    private int phone;
+    private int money;
+    private int age;
 
+
+    public persson( String FIO, String position, String email , int phone, int money, int age){
+        this.FIO = FIO;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
 
 
 }
+
+
+
