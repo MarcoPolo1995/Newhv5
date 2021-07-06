05fe01c20077024ae79ec04773a7932ffb4758bb
marco_polo <nol-95@mail.ru>
2021-07-06 17:36:57 +0300

    f

--- src/lesso5/persson.java
+++ src/lesso5/persson.java
@@ -27,5 +27,28 @@ public class persson {
 
 }
 
+public class mainhw5class {
+    public static void main(String[] args) {
+        persson[] staff = new persson[5];
+        staff[0]= new persson("Ivanov Ivan", "disagner", "tol@mail.ru",6745,30000,25);
+        staff[1]= new persson("Petrov Petr", "master", "tyu@mail.ru", 8906, 10000,47);
+        staff[2]= new persson("Ivanov Pavel", "admin","bik@mail.ru",23423,30000,41);
+        staff [3]= new persson("Anatoli Lop", "computer","sxce@mail.ru", 345345, 345, 47);
+        staff [4]= new persson("Nikolai Nikolaev", "director","dir@mail.ru",90007, 1000000, 41);
+
+
+        for (int i = 0; i < staff.length; i++) {
+            if (staff[i].getAge()>40){
+                System.out.println(staff[i]);
+            }
+
+        }
+
+
+    }
+
+
+}
+
 
 
